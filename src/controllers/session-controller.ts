643cfc156d8f844916c71e9676c20ada31b77956
Ruan Victor <ruanvclima@gmail.com>
2026-09-11 09:01:32 -0300

    feat: received and validated user to create a new session

--- src/controllers/session-controller.ts
+++ src/controllers/session-controller.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { z } from "zod"
+import { AppError } from "@/utils/AppError"
  
 class SessionController {
-    create(request: Request, response: Response){
+    async create(request: Request, response: Response){
+
+        const bodySchema = z.object({
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+
+        const { email, password } = bodySchema.parse(request.body)
+
+        const user = await prisma.user.findFirst({
+            where: {email}
+        })
+
+        if (!user){
+            throw new AppError("Invalid email or password", 401)
+        }
 
 
         return response.json({message: "ok"})
