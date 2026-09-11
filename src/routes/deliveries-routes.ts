cdb00ccebd64ef626110413b5341f1716d452a6a
Ruan Victor <ruanvclima@gmail.com>
2026-09-11 20:53:10 -0300

    feat: verified the authorization of user

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -3,12 +3,12 @@ import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveries-controllers";
 
 import { ensureAuthenticator } from "@/middlewares/ensure-authenticator";
+import { verifyUserAuthorization } from "@/middlewares/verifyuserAuthorization";
 
 const deliveriesRoutes = Router()
 const deliveriesController =  new DeliveriesController()
 
-deliveriesRoutes.use( ensureAuthenticator)
-
+deliveriesRoutes.use( ensureAuthenticator, verifyUserAuthorization(["sale"]))
 
 deliveriesRoutes.post("/", deliveriesController.create)
 
