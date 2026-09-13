2cff7b944692db9305e4e219371a2070198a86bf
Ruan Victor <ruanvclima@gmail.com>
2026-09-13 16:26:05 -0300

    feat: updated the status from delivery

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,16 +1,22 @@
 import { Router } from "express";
 
 import { DeliveriesController } from "@/controllers/deliveries-controllers";
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller";
 
 import { ensureAuthenticator } from "@/middlewares/ensure-authenticator";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
+
 const deliveriesRoutes = Router()
 const deliveriesController =  new DeliveriesController()
+const deliveriesStatusController = new DeliveriesStatusController()
 
 deliveriesRoutes.use( ensureAuthenticator, verifyUserAuthorization(["sale"]))
 
 deliveriesRoutes.post("/", deliveriesController.create)
 deliveriesRoutes.get("/", deliveriesController.index)
+
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update)
 
 export {deliveriesRoutes}
