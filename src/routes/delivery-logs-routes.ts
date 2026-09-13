acf4e80a3ccacba681fd901c4df463e265aaef9e
Ruan Victor <ruanvclima@gmail.com>
2026-09-13 17:02:22 -0300

    feat: created the show function that shows the user delivery details

--- src/routes/delivery-logs-routes.ts
+++ src/routes/delivery-logs-routes.ts
@@ -16,4 +16,11 @@ deliveryLogsRoutes.post(
     deliveryLogsController.create
 )
 
+deliveryLogsRoutes.get(
+    "/:delivery_id/show", 
+    ensureAuthenticator,
+    verifyUserAuthorization(["sale", "customer"]),
+    deliveryLogsController.show
+)
+
 export {deliveryLogsRoutes}
