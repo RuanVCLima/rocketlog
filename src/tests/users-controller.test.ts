58ac8ff460e804d742c81d01515d7b5afcd842b1
Ruan Victor <ruanvclima@gmail.com>
2026-09-14 09:41:19 -0300

    feat: added a test to verify if the email is valid

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -41,4 +41,15 @@ describe("UsersController",()=>{
 
 
     })
+
+    it("should throw a validation error if email is invalid", async () =>{
+        const response = await request(app).post("/users").send({
+            name: "Test User",
+            email: "invalid-email",
+            password: "password123"
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("validation error")
+    })
 })
